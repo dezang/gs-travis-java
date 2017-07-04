7c5bad192db3af178f8d80cd91c2ce5d60f72cbe
hwangdezang <hwangdezang@woowahan.com>
2017-07-04 14:33:11 +0900

    스프링 부트 추가

---
+++ src/main/java/net/dezang/web/HomeRestController.java
@@ -0,0 +1,13 @@
+package net.dezang.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+    @GetMapping("/")
+    public ResponseEntity<?> home() {
+        return ResponseEntity.ok("Hello");
+    }
+}
