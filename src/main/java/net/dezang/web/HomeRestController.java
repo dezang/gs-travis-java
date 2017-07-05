0fbeea76a4a67f4278bb918c9ca0882536b609f4
dezang <dezang@dezang.net>
2017-07-05 16:51:20 +0900

    edit HomeController

--- src/main/java/net/dezang/web/HomeRestController.java
+++ src/main/java/net/dezang/web/HomeRestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeRestController {
     @GetMapping("/")
     public ResponseEntity<?> home() {
-        return ResponseEntity.ok("Hello");
+        return ResponseEntity.ok("Hello, Dezang.");
     }
 }
